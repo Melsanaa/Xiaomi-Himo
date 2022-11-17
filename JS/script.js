492b9fde61bbb734e7e2af82f4fdaecf45f5449a
Melsanaa <fjord.melikhova@yandex.ru>
2022-11-17 18:26:40 +0500

    2

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,16 @@
 $(function(){
-	$('.bike-slider').slick({
+	$('.bike-slider, .slider__items').slick({
 		arrows: false,
 		dots: true,
 		fade: true,
 		autoplay: true,
 		autoplaySpeed: 5000,
+	});
+
+	$(".menu, .bike__column").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
 	});
 });
